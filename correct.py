4ea8b274d11896510251cb25717c858f848ca6d8
Edward Betts <edwardbetts@gmail.com>
2011-11-14 20:43:23 +0000

    fixes and features

--- correct.py
+++ correct.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for, g, Respons
 from lxml import etree
 from urllib import urlopen
 import MySQLdb, urllib, json
-from lxml import etree
 from decimal import Decimal
+from pprint import pprint
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = open('/home/edward/src/corrections/secret_key').read()[-1]
 
 def group_words(fmt):
     cur = []
@@ -73,6 +75,7 @@ def leaf(identifier, leaf):
     item = get_item(identifier)
     host, path = locate(identifier)
     url = 'http://%s/~edward/get_leaf.php?item_id=%s&doc=%s&path=%s&leaf=%d' % (host, identifier, identifier, path, leaf)
+    print url
     page = etree.parse(url).getroot()
     page_w = int(page.get('width'))
     lines = []
@@ -85,6 +88,11 @@ def leaf(identifier, leaf):
             if len(par) == 0 or len(par[0]) == 0 or len(par[0][0]) == 0:
                 continue
             for line in par:
+                char_offset = 0
+                for fmt in line:
+                    for c in fmt:
+                        c.set('char_offset', str(char_offset))
+                        char_offset += 1
                 l = int(line.get('l'))
                 if not text_l or l < text_l:
                     text_l = l
@@ -93,10 +101,10 @@ def leaf(identifier, leaf):
                     text_r = r
                 lines.append((int(line.get('t')), int(line.get('b')), line))
 
-    text_w = text_r-text_l if (text_r is not None and text_l is not None) else None
+    text_w = text_r-text_l if (text_r is not None and text_l is not None) else 0
     return render_template('leaf.html', item=item, leaf=leaf, lines=lines, \
             page_w=page_w, int=int, Decimal=Decimal, \
-            text_x=text_l, text_w=text_w, group_words=group_words)
+            text_x=text_l, text_w=text_w, group_words=group_words, max=max, len=len)
 
 def get_item(identifier):
     cur = g.db.cursor(MySQLdb.cursors.DictCursor)
@@ -109,6 +117,56 @@ def get_item(identifier):
 def item(identifier):
     item=get_item(identifier)
     return render_template('item.html', item=item, int=int)
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/save/<identifier>/<int:page>", methods=['POST'])
+def save(identifier, page):
+    edits = []
+    for k, v in request.form.iteritems():
+        if not k.startswith('word_'):
+            continue
+        line, char = map(int, k.split('_')[1:])
+        edits.append((identifier, page, line, char, v))
+    for e in sorted(edits):
+        print e
+    return ''
+
+ns = '{http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml}'
+page_tag = ns + 'page'
+
+@app.route("/view/<identifier>")
+def view(identifier):
+    item=get_item(identifier)
+    host, path = locate(identifier)
+    url = 'http://%s/~edward/read_abbyy.php?item_id=%s&doc=%s&path=%s' % (host, identifier, identifier, path)
+    f = urlopen(url)
+    page_count = 0
+    body = []
+    for eve, page in etree.iterparse(f):
+        if page.tag != page_tag:
+            continue
+        for block in page:
+            if block.attrib['blockType'] != 'Text':
+                continue
+            region, text = block
+            for par in text:
+                cur_par = ''
+                if len(par) == 0 or len(par[0]) == 0 or len(par[0][0]) == 0:
+                    continue
+                for line in par:
+                    chars = []
+                    for fmt in line:
+                        chars += [c.text for c in fmt]
+                    cur_par += ''.join(chars)
+                body.append(cur_par)
+        if page_count == 20:
+            break
+        page_count += 1
+
+    return render_template('view.html', item=item, int=int, body=body)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
